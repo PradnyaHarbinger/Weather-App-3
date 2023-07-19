c70cf81ce9190fd3ab6e3fe540e0b83cd7add665
PradnyaHarbinger <139337303+PradnyaHarbinger@users.noreply.github.com>
2023-07-19 10:08:22 +0530

    Initial

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    //https://rapidapi.com/KirylBokiy/api/openweather43/
+    production: false,
+    // weatherApiBaseUrl: 'https://open-weather13.p.rapidapi.com/',
+    
+    
+    weatherApiBaseUrl: 'https://openweather43.p.rapidapi.com/weather',
+    XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+    XRapidAPIHostHeaderValue: 'openweather43.p.rapidapi.com',
+    XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+    XRapidAPIKeyHeaderValue: '81f797c817msh2587b408e3af511p1586d0jsn6a926b4d9cc9'
+
+};
